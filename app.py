bc39675eba8c69b49784cc18c4867f84d00d73ba
Timothy Pelosi <9578200+TimothyPelosi@users.noreply.github.com>
2024-10-10 16:18:19 -0400

    Add comments

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
-# Initialize SQLite database
+# Initialize SQLite database.
 def init_db():
     conn = sqlite3.connect('expenses.db')
     cur = conn.cursor()
@@ -13,18 +13,22 @@ def init_db():
     conn.commit()
     conn.close()
 
+# Create index route.
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Create route to add expenses to DB manually.
 @app.route('/add', methods=['GET', 'POST'])
 def add_expense():
+    # Handles form submissions.
     if request.method == 'POST':
         date = request.form['date']
         description = request.form['description']
         category = request.form['category']
         price = request.form['price']
         
+        # Connect to database and insert from form into database.
         conn = sqlite3.connect('expenses.db')
         cur = conn.cursor()
         cur.execute("INSERT INTO expenses (Date, Description, Category, Price) VALUES (?, ?, ?, ?)",
@@ -36,11 +40,13 @@ def add_expense():
     
     return render_template('add.html')
 
+# Create finances summary route.
 @app.route('/summary', methods=['GET', 'POST'])
 def view_summary():
     conn = sqlite3.connect('expenses.db')
     cur = conn.cursor()
 
+    # Choose time month and year for summary.
     if request.method == 'POST':
         month = request.form['month']
         year = request.form['year']
@@ -48,6 +54,7 @@ def view_summary():
         expenses = cur.fetchall()
         cur.execute("SELECT SUM(Price) FROM expenses WHERE strftime('%m', Date) = ? AND strftime('%Y', Date) = ?", (month, year))
         total_expense = cur.fetchone()[0]
+    # If not POST then retrieve sum of prices and stores in total.
     else:
         cur.execute("SELECT * FROM expenses")
         expenses = cur.fetchall()
@@ -58,14 +65,17 @@ def view_summary():
     
     return render_template('summary.html', expenses=expenses, total=total_expense)
 
+# Create a route to search.
 @app.route('/search')
 def search():
     return render_template('search.html')
 
+# Create a route to upload csv from Bank of America.
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_csv():
     if request.method == 'POST':
         file = request.files['file']
+        # Use pandas to read csv, skip unnecessary rows, iterate through and grab relevant data for local database.
         if file:
             df = pd.read_csv(file, skiprows=5)
             conn = sqlite3.connect('expenses.db')
@@ -81,6 +91,7 @@ def upload_csv():
             return redirect(url_for('index'))
     return render_template('upload.html')
 
+# Create route to edit database entries individually.
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_expense(id):
     conn = sqlite3.connect('expenses.db')
